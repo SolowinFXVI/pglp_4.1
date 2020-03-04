6e6ce69d7dedf72b93c9b61b596f5fc429299e91
SolowinFXVI <julien.n.jacquet@gmail.com>
2020-03-04 11:34:53 +0100

    created composite

---
+++ src/main/java/org/uvsq21400579/CompositePersonnels.java
@@ -0,0 +1,23 @@
+package org.uvsq21400579;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositePersonnels implements InterfacePersonnels{
+    private List<Personnel> personnelsList = new ArrayList<Personnel>();
+
+    public void add(Personnel personnel){
+        personnelsList.add(personnel);
+    }
+
+    public void remove(Personnel personnel){
+        personnelsList.remove(personnel);
+    }
+
+    @Override
+    public void print() {
+        for(Personnel personnel : personnelsList){
+            System.out.println(personnel);
+        }
+    }
+}
